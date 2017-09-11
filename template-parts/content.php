2021ff9a4c51eebd55f9cbbbdaf8f4ab9d6726e9
renventura <rmvohio@gmail.com>
2017-09-10 21:25:02 -0400

    Iterating and bug fixes

--- template-parts/content.php
+++ template-parts/content.php
@@ -13,6 +13,10 @@
 	
 	<header class="entry-header">
 		<?php
+		if ( ! is_single() ) {
+			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+		}
+
 		if ( 'post' === get_post_type() ) : ?>
 		<div class="entry-meta row middle-xs center-xs">
 			<?php personaltheme_posted_on(); ?>
